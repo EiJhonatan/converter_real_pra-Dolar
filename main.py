097811a599f65711bfa6d2d79e6cbf070607a427
jhonatan ferreira <devjhow2003@gmail.com>
2025-01-03 13:45:46 -0300

    bug-fix(OK)

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ print('Programa para calcucular REAL para DÓLAR')
 print()
 
 real = float(input('Digite um valor em reais: '))
+print()
 
 dolar = float(input('Digite o valor do DÓLAR atual (em REAIS): '))
+print()
 
 valor_convert = real/dolar
 print('R$ {0:.2f} valem: U$ {1:.2f}'.format(real,valor_convert))
